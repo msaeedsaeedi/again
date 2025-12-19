0ae49bc013fb0a7e3c67331e991ffeebf6e9a0ac
msaeedsaeedi <msaeedsaeedi@outlook.com>
2025-12-19 12:29:59 +0500

    feat: sequential executor added

---
+++ internal/app/executor.go
@@ -0,0 +1,46 @@
+package app
+
+import (
+	"context"
+
+	"github.com/msaeedsaeedi/again/internal/domain"
+	"github.com/msaeedsaeedi/again/internal/infra"
+)
+
+type ResultHandler interface {
+	OnStart(runID int)
+	OnComplete(result domain.RunResult)
+	OnFinish()
+}
+
+type Executor interface {
+	Execute(ctx context.Context, cfg *domain.RunConfig, handler ResultHandler) error
+}
+
+type SequentialExecutor struct {
+	runner *infra.CommandRunner
+}
+
+func NewSequentialExecutor(runner *infra.CommandRunner) *SequentialExecutor {
+	return &SequentialExecutor{
+		runner: runner,
+	}
+}
+
+func (e *SequentialExecutor) Execute(ctx context.Context, cfg *domain.RunConfig, handler ResultHandler) error {
+	results := make([]domain.RunResult, 0, cfg.Times)
+
+	for i := 1; i <= cfg.Times; i++ {
+		handler.OnStart(i)
+		result := e.runner.Run(ctx, cfg, i)
+		results = append(results, result)
+		handler.OnComplete(result)
+	}
+
+	handler.OnFinish()
+	return nil
+}
+
+func NewExecutor(cfg *domain.RunConfig, runner *infra.CommandRunner) Executor {
+	return NewSequentialExecutor(runner)
+}
